8b3f373da949fc90a751f57ccce5f839ee9942b2
杨林 <yanglin001@zhongan.com>
2018-12-10 21:25:19 +0800

    开始写demo

---
+++ src/components/HelloReact.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+class HelloReact extends React.Component {
+    render() {
+        return (<div>
+            test
+        </div> )
+    }
+}
+export default HelloReact;
---
+++ src/components/WriteData.js
@@ -0,0 +1,93 @@
+
+import React from 'react';
+import { Button } from 'antd';
+
+
+class WriteData extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inComeData: [
+                {
+                    year: '2018',
+                    monthsData: [
+                        {
+                            monthMoney: '14000',
+                            months: 12
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+    addYearsEvents(j) {
+        let { inComeData } = this.state;
+        inComeData = JSON.parse(JSON.stringify(inComeData))
+        inComeData[j].monthsData.push({
+            monthMoney: '18000',
+            months: 12
+        })
+        this.setState({
+            ...this.state,
+            inComeData
+        })
+    }
+    changeYear(i,e) {
+        let { inComeData } = this.state;
+        inComeData = JSON.parse(JSON.stringify(inComeData));
+        inComeData[i].year = e.target.value;
+        this.setState({
+            ...this.state,
+            inComeData
+        })
+    }
+    changeMoths(i,j, e) {
+        let { inComeData } = this.state;
+        inComeData = JSON.parse(JSON.stringify(inComeData));
+        inComeData[i].monthsData[j].months = e.target.value;
+        this.setState({
+            ...this.state,
+            inComeData
+        })
+    }
+    changeMonthMoney(i,j,e) {
+        let { inComeData } = this.state;
+        inComeData = JSON.parse(JSON.stringify(inComeData));
+        inComeData[i].monthsData[j].monthMoney = e.target.value;
+        this.setState({
+            ...this.state,
+            inComeData
+        })
+    }
+    render() {
+        return (<div>
+            {this.state.inComeData.map((e, i) => {
+                return (
+                    <div key={i}>
+                        <div >
+                            年份：<input type="text" value={e.year} onChange={this.changeYear.bind(this, i)} />
+                        </div>
+                        {
+                            e.monthsData.map((env, j) => {
+                                return (
+                                    <div key={j}>
+                                        月薪
+                                        <label>
+                                            <input type="text" placeholder="请输入月薪" value={env.monthMoney} onChange={this.changeMonthMoney.bind(this, i,j)} />
+                                        </label>
+                                        <span > X </span>
+                                        <label>
+                                            <input type="text" placeholder='请输入月薪的月数' value={env.months} onChange={this.changeMoths.bind(this, i,j)} />
+                                        </label>
+                                        {e.monthsData.length == j + 1 ? <Button type="primary" onClick={this.addYearsEvents.bind(this,i)}>Add</Button>:null}
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                )
+            })}
+        </div>)
+    }
+}
+export default WriteData;
